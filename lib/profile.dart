94b38d052d79af7426855f375038898ca80cf4fa
Aman <aman.bansal@mail.vinove.com>
2025-05-12 12:57:02 +0530

    Batch-1

---
+++ lib/profile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+// import 'package:to_do_app/home.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(children: <Widget>[Text('Profile Page')]),
+    );
+  }
+}
